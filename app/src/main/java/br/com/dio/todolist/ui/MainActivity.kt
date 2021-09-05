76ba6daef338d8fb5a2217ab4bbeac593e6c578a
Ezequiel Messore <ezequiel.messore@hotmail.com>
2021-09-05 15:33:31 -0300

    Remover o uso do startActityForResult.

--- app/src/main/java/br/com/dio/todolist/ui/MainActivity.kt
+++ app/src/main/java/br/com/dio/todolist/ui/MainActivity.kt
@@ -3,8 +3,8 @@ package br.com.dio.todolist.ui
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import br.com.dio.todolist.databinding.ActivityMainBinding
 import br.com.dio.todolist.datasource.TaskDataSource
@@ -13,6 +13,15 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val adapter by lazy { TaskListAdapter() }
+
+    /**
+     * Nova maneira de iniciar uma activity.
+     * Já que `startActivityForResult` foi depreciado.
+     */
+    private val register =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) updateList()
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,30 +32,23 @@ class MainActivity : AppCompatActivity() {
         updateList()
 
         insertListeners()
-        //DATA STORE
-        //ROOM
     }
 
     private fun insertListeners() {
         binding.fab.setOnClickListener {
-            startActivityForResult(Intent(this, AddTaskActivity::class.java), CREATE_NEW_TASK)
+            register.launch(Intent(this, AddTaskActivity::class.java))
         }
 
         adapter.listenerEdit = {
             val intent = Intent(this, AddTaskActivity::class.java)
             intent.putExtra(AddTaskActivity.TASK_ID, it.id)
-            startActivityForResult(intent, CREATE_NEW_TASK)
+            register.launch(intent)
         }
 
         adapter.listenerDelete = {
             TaskDataSource.deleteTask(it)
             updateList()
         }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == CREATE_NEW_TASK && resultCode == Activity.RESULT_OK) updateList()
     }
 
     private fun updateList() {
@@ -56,9 +58,4 @@ class MainActivity : AppCompatActivity() {
 
         adapter.submitList(list)
     }
-
-    companion object {
-        private const val CREATE_NEW_TASK = 1000
-    }
-
 }
